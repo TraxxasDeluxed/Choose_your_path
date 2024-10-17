34cf5fed662a2d638794b3e92dffbe90ed8c9bcc
TraxxasDeluxed <144713587+TraxxasDeluxed@users.noreply.github.com>
2024-10-17 16:58:45 +0000

    added the correct base

--- main.java
+++ main.java
@@ -1,33 +1,49 @@
+import java.lang.management.BufferPoolMXBean;
 import java.util.Scanner;
 
 public class main {
     
     public static void main(String[] args) {
-        String Path1;
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("You woke up in the middle of the Tam, you want to go back to Redwood");
+        System.out.println("You woke up in the middle of the Tam, you want to go back to Redwood, do you want to go back? yes or no");
 
-        String input = scanner.nextLine().trim().toLowerCase();
-        boolean choice = evaluateChoice(input);
+      
 
-        String result = choosePath1(choice);
-        System.out.println(result);
+        //System.out.println(choice);
 
+        if (choosechoice()) {
+            System.out.println("You find that there only 2 paths to reach Redwood, the river or the road, do you want to continue embarking to Redwood? yes or no");
+            if (choosechoice()){
+                System.out.println("As you walk, the air becomes cooler, and the sounds of the forest shift. After a while, you come across an old, crumbling stone marker. Do you want to explore it?");
+                if (choosechoice()){
+                    System.out.println("You feel drawn to it, sensing it might hold clues about the forest.");
+                }else{
+                    System.out.println("You push on, determined to reach your destination.");
+                }
+            }else{
+                System.out.println("You find a deserted car? do you drive it or not");
+            }
+        } 
+        else {
+           System.out.println("You feel compelled to explore the Tam further, intrigued by its secrets.");
+        }
         scanner.close();
     }
 
-    public static boolean evaluateChoice(String input) {
+   
+
+    public static boolean choosechoice(){
+        Scanner scanner = new Scanner(System.in);
+       String input = scanner.nextLine().trim().toLowerCase();
         return input.equals("yes");
+        
     }
 
-    public static String choosePath1(boolean choice) {
+    
       
-        if (choice) {
-            System.out.println("Hello");
-        } 
-        else {
-           System.out.println("Bello");
-        }return null;
-    }
+   
+    
+
+   
 }
